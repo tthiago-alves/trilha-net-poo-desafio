8610853abc6328cf46e6b4bf89be80284688ae0e
Thiago Alves <thiagoa748@gmail.com>
2024-10-14 17:49:19 -0300

    Desafio de POO finalizado com sucesso!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set;}
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smarthphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11947631483", modelo: "Nokia", imei: "123456", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smarthphone Iphone:");
+Smartphone iphone = new Iphone(numero: "11947631488", modelo: "Iphone", imei: "1234567", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
